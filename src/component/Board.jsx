562a57e6eeec0e0e23d3b9012f0b49ee1e61b708
AnwarNa <anwar.se19@gmail.com>
2024-03-05 07:42:12 +0300

    build the board

---
+++ src/component/Board.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Board() {
+  return (
+<div className="container">
+<div className="white "></div>
+<div className="white left-right"></div>
+<div className="white "></div>
+<div className="white top-bottom"></div>
+<div className="white-5"></div>
+<div className="white top-bottom"></div>
+<div className="white "></div>
+<div className="white left-right"></div>
+<div className="white "></div>
+</div>
+  );
+}
+
+export default Board;
